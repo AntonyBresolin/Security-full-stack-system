ed77cdc5a7492d67e972f679110d320a5dba676b
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-07-09 20:46:34 -0300

    initial structure backend and frontend
    Utilizado como base o backend do projeto de spring_security_jwt_oauth_2.

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div className="text-2xl text-center mt-4 text-red-500">Hello, World!</div>
+    </>
+  );
+}
+
+export default App;
